0c0022feae563971340da9357fe8d8aba7c5bc72
Azam Gulov <gulovazam@gmail.com>
2023-07-01 20:51:12 +0500

    Add ship factory
    add ship metods
    pass tests

---
+++ src/modules/ship.js
@@ -0,0 +1,23 @@
+const Ship = (length) => {
+  const ship = {
+    length,
+    hitCount: 0,
+    isSunk: false,
+  };
+
+  const hit = () => {
+    ship.hitCount += 1;
+  };
+
+  const isSunk = () => ship.length === ship.hitCount;
+
+  return {
+    hit,
+    isSunk,
+    get info() {
+      return ship;
+    },
+  };
+};
+
+export default Ship;
