4592eedd91760816a5356052c86c4aba00b9741d
Gulov Azam <gulovazam@gmail.com>
2023-07-02 00:41:15 +0500

    Create separate ship instance for each unit test

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,20 +1,22 @@
 import Ship from "../modules/ship";
 
-const ship = Ship(1);
-
 test("Ship properties include: length, hit count and status", () => {
+  const ship = Ship(5);
   expect(ship.info).toMatchObject({
-    length: 1,
+    length: 5,
     hitCount: 0,
     isSunk: false,
   });
 });
 
 test("Hit counter", () => {
+  const ship = Ship(2);
   ship.hit();
   expect(ship.info.hitCount).toBe(1);
 });
 
 test("Ship sunk status", () => {
+  const ship = Ship(1);
+  ship.hit();
   expect(ship.isSunk()).toBe(true);
 });
