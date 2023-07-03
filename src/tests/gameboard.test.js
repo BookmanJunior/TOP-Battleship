f9ad7ec13ca5633f57974180778641cc50fbf103
Azam Gulov <gulovazam@gmail.com>
2023-07-03 15:50:32 +0500

    Add test to confirm correct ship placement

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -12,3 +12,16 @@ gameboard.board.forEach((row) => {
     expect(row.length).toBe(10);
   });
 });
+
+test("Given ship length and starting coordinates return its coordinates on the board", () => {
+  const placingBoard = Gameboard();
+  const ship = Ship(5);
+  placingBoard.placeShip(ship, [0, 0]);
+  expect(ship.info.coordinates).toStrictEqual([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+  ]);
+});
