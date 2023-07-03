d20ad59ceb6485169704b875eabe959689f164dc
Azam Gulov <gulovazam@gmail.com>
2023-07-03 16:29:00 +0500

    Add tests

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -25,3 +25,19 @@ test("Given ship length and starting coordinates return its coordinates on the b
     [0, 4],
   ]);
 });
+
+test("Prevents ship placement if ship length goes off board", () => {
+  const offBoard = Gameboard();
+  const offBoardShip = Ship(5);
+  offBoard.placeShip(offBoardShip, [0, 6]);
+  expect(offBoardShip.info.coordinates).toStrictEqual([]);
+});
+
+test("Prevents ship placement if ships exists on given coordinates", () => {
+  const newBoard = Gameboard();
+  const Overlappingship1 = Ship(4);
+  const Overlappingship2 = Ship(5);
+  newBoard.placeShip(Overlappingship1, [1, 0]);
+  newBoard.placeShip(Overlappingship2, [1, 3]);
+  expect(Overlappingship2.info.coordinates).toStrictEqual([]);
+});
