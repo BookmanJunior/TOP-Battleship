628755b8c4c7b1564d38da7d7228a962f441e33e
Azam Gulov <gulovazam@gmail.com>
2023-07-03 15:54:45 +0500

    Add placeShip method
    pass test

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,5 +1,7 @@
 const Gameboard = () => {
   const gameboard = generateBoard();
+  const ships = [];
+  const occupiedSquares = [];
 
   function generateBoard() {
     const arr = [];
@@ -12,7 +14,21 @@ const Gameboard = () => {
     return arr;
   }
 
+  const placeShip = (ship, startingCoor) => {
+    const coors = ship.info.coordinates;
+    coors.push(startingCoor);
+    occupiedSquares.push(startingCoor);
+
+    for (let i = 1; i < ship.info.length; i++) {
+      const newCoordinates = [startingCoor[0], startingCoor[1] + i];
+      coors.push(newCoordinates);
+      occupiedSquares.push(newCoordinates);
+    }
+    ships.push(ship);
+  };
+
   return {
+    placeShip,
     get board() {
       return gameboard;
     },
