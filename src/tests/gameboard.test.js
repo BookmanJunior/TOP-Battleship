49b79965d99941c4ac49ca032b699ac2775cc842
Azam Gulov <gulovazam@gmail.com>
2023-07-03 15:38:14 +0500

    Add tests

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,1 +1,14 @@
 import Gameboard from "../modules/gameboard";
+import Ship from "../modules/ship";
+
+const gameboard = Gameboard(Ship);
+
+test("Gameboard has 10 rows", () => {
+  expect(gameboard.board.length).toBe(10);
+});
+
+gameboard.board.forEach((row) => {
+  test("Each gameboard row has 10 squares", () => {
+    expect(row.length).toBe(10);
+  });
+});
