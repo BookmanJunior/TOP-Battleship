c5aa960d280d187c2cd9369355d008fe2de56efa
Azam Gulov <gulovazam@gmail.com>
2023-07-03 15:45:36 +0500

    Add generateBoard method
    pass tests

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,3 +1,22 @@
-const Gameboard = () => {};
+const Gameboard = () => {
+  const gameboard = generateBoard();
+
+  function generateBoard() {
+    const arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push([]);
+      for (let j = 0; j < 10; j++) {
+        arr[i].push([]);
+      }
+    }
+    return arr;
+  }
+
+  return {
+    get board() {
+      return gameboard;
+    },
+  };
+};
 
 export default Gameboard;
