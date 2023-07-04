7aaa4803316890416316deeb68b56507cf28f529
Azam Gulov <gulovazam@gmail.com>
2023-07-04 12:20:14 +0500

    Add vertical placement tests

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -41,3 +41,25 @@ test("Prevents ship placement if ships exists on given coordinates", () => {
   newBoard.placeShip(Overlappingship2, [1, 3]);
   expect(Overlappingship2.info.coordinates).toStrictEqual([]);
 });
+
+test("Given ship length and starting coordinates place ship vertically and return its coordinates", () => {
+  const verticalBoard = Gameboard();
+  const ship = Ship(5);
+  verticalBoard.changePlacementPlane();
+  verticalBoard.placeShip(ship, [0, 0]);
+  expect(ship.info.coordinates).toStrictEqual([
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 0],
+  ]);
+});
+
+test("Prevents vertical ship placement if ship length goes off board", () => {
+  const offBoardVertically = Gameboard();
+  const offBoardShip = Ship(5);
+  offBoardVertically.changePlacementPlane();
+  offBoardVertically.placeShip(offBoardShip, [6, 0]);
+  expect(offBoardShip.info.coordinates).toStrictEqual([]);
+});
