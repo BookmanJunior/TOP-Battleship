66ac2ae80bc1571eb898f367cff7e47c6b549333
Azam Gulov <gulovazam@gmail.com>
2023-07-07 20:15:21 +0500

    Add devtool and devserver
    fix rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 const path = require("path");
 
 module.exports = {
-  entry: ".src/index.js",
+  mode: "development",
+  entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: ["node_modules"],
-        use: ["babel-loader"],
+        exclude: "/node_modules/",
+        use: { loader: "babel-loader" },
       },
     ],
   },
