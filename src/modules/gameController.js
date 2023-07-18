695f967c7377693f60a8a9d460b4926bcc009171
Azam Gulov <gulovazam@gmail.com>
2023-07-18 11:39:50 +0500

    Initialize players and gameboards
    Add gameState and gameModes

--- src/modules/gameController.js
+++ src/modules/gameController.js
@@ -1,3 +1,24 @@
-const GameController = () => {};
+import Gameboard from "./gameboard";
+import Ship from "./ship";
+import Player from "./player";
+
+const GameController = () => {
+  const player1Gameboard = Gameboard("player1-gameboard", Ship);
+  const player2Gameboard = Gameboard("player2-gameboard", Ship);
+
+  const player1 = Player(player2Gameboard);
+  const player2 = Player(player1Gameboard);
+
+  const gameState = {
+    playing: "playing",
+    "placing-ship": "placing-ship",
+    gameover: "gameover",
+  };
+
+  const gameModes = {
+    ai: "ai",
+    player: "player",
+  };
+};
 
 export default GameController;
