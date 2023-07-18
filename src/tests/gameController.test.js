a439c0752f290bd8707bbea0a293cf99805c6005
Azam Gulov <gulovazam@gmail.com>
2023-07-18 11:36:48 +0500

    Add test
    Modify the first test

--- src/tests/gameController.test.js
+++ src/tests/gameController.test.js
@@ -5,6 +5,14 @@ test("Game Over when one player loses all ships", () => {
 
   game.player1Gameboard.placeShip(5, [0, 0]);
   game.player2Gameboard.placeShip(1, [0, 0]);
+  game.changeState();
   game.playRound([0, 0]);
-  expect(game.gameOn).toBe(false);
+  expect(game.state).toBe("gameover");
+});
+
+test("Check if game is ready to start against AI if player1 placed all ships on the gameboard", () => {
+  const game = GameController();
+  game.player1Gameboard.randomizeShipPlacement();
+  game.player2Gameboard.randomizeShipPlacement();
+  expect(game.isReadyToStart()).toBe(true);
 });
