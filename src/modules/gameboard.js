44ed265fd42475b29940fd49d5cc47eb9a085451
Azam Gulov <gulovazam@gmail.com>
2023-07-21 13:14:49 +0500

    Add method that returns ships adjacent cells
    update isShipDuplicate method
    change receiveAttack method to return object instead of array if ship was sunk

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -3,9 +3,9 @@ import { generateRandomCoordinates } from "./coordinateGenerator";
 const Gameboard = (name, shipMaker) => {
   const ships = [];
   const attackedSquares = [];
+  let isVertical = false;
   // used to create ship
   const availableShips = [5, 4, 3, 2, 2];
-  let isVertical = false;
 
   const placeShip = (length, startingCoor) => {
     if (!isValidPlacement(startingCoor)) return;
@@ -17,19 +17,20 @@ const Gameboard = (name, shipMaker) => {
     if (newCoordinates) {
       shipInfo.coordinates.push(...newCoordinates);
       ships.push(ship);
+      getAdjacentSquares(ship);
       return ship;
     }
   };
 
   const receiveAttack = (coordinates) => {
-    const isShip = isShipCoordinate(coordinates);
+    const isShip = isShipCoordinate("coordinates", coordinates);
     if (isShip.length) {
       isShip[0].hit();
       // change ships sunk status if it was sunk
       isShip[0].info.isSunk = !!isShip[0].isSunk();
       attackedSquares.push(coordinates);
 
-      return isShip[0].isSunk() ? isShip[0].info.coordinates : "hit";
+      return isShip[0].isSunk() ? isShip[0].info : "hit";
     }
     attackedSquares.push(coordinates);
     return "miss";
@@ -62,17 +63,81 @@ const Gameboard = (name, shipMaker) => {
     return allCoors;
   }
 
+  function getAdjacentSquares(ship) {
+    const possibleAdjacentSquares = [
+      [-1, 1],
+      [-1, -1],
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+      [1, 1],
+      [1, -1],
+    ];
+
+    const shipCoors = ship.info.coordinates;
+    for (let i = 0; i < shipCoors.length; i++) {
+      const currentXCoor = shipCoors[i][0];
+      const currentYCoor = shipCoors[i][1];
+
+      if (!isVertical) {
+        if (i === 0 && currentYCoor - 1 >= 0) {
+          ship.info.adjacentSquares.push([currentXCoor, currentYCoor - 1]);
+        }
+
+        if (i === shipCoors.length - 1 && currentYCoor + 1 <= 9) {
+          ship.info.adjacentSquares.push([currentXCoor, currentYCoor + 1]);
+        }
+      } else {
+        if (i === 0 && currentXCoor - 1 >= 0) {
+          ship.info.adjacentSquares.push([currentXCoor - 1, currentYCoor]);
+        }
+
+        if (i === shipCoors.length - 1 && currentXCoor + 1 <= 9) {
+          ship.info.adjacentSquares.push([currentXCoor + 1, currentYCoor]);
+        }
+      }
+
+      for (let j = 0; j < possibleAdjacentSquares.length; j++) {
+        const newXCoor = possibleAdjacentSquares[j][0] + currentXCoor;
+        const newYCoor = possibleAdjacentSquares[j][1] + currentYCoor;
+
+        if (
+          isOnBoard([newXCoor, newYCoor]) &&
+          !notDuplicate(ship.info.adjacentSquares, [newXCoor, newYCoor]) &&
+          newXCoor !== currentXCoor &&
+          newYCoor !== currentYCoor
+        ) {
+          ship.info.adjacentSquares.push([newXCoor, newYCoor]);
+        }
+      }
+    }
+  }
+
+  function notDuplicate(shipAdjCoors, coors) {
+    return shipAdjCoors.find((c) => c.every((s, index) => s === coors[index]));
+  }
+
   function isValidPlacement(coordinates) {
-    return isOnBoard(coordinates) && !isShipCoordinate(coordinates).length;
+    return (
+      isOnBoard(coordinates) &&
+      !isShipCoordinate("coordinates", coordinates).length &&
+      !isShipCoordinate("adjacentSquares", coordinates).length
+    );
   }
 
   function isOnBoard(coordinates) {
-    return coordinates[0] <= 9 && coordinates[1] <= 9;
+    return (
+      coordinates[0] >= 0 &&
+      coordinates[0] <= 9 &&
+      coordinates[1] >= 0 &&
+      coordinates[1] <= 9
+    );
   }
 
-  function isShipCoordinate(coordinates) {
+  function isShipCoordinate(arrayToCheck, coordinates) {
     return ships.filter((ship) => {
-      const shipsCoor = ship.info.coordinates;
+      const shipsCoor = ship.info[arrayToCheck];
       return shipsCoor.find((coor) =>
         coor.every((c, index) => c === coordinates[index])
       );
